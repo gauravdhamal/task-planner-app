faab178c16c054d685304f17ef38234e6f79e5d7
gauravdhamal <gauravdhamal11@gmail.com>
2023-03-24 14:13:25 +0530

    Swagger implemented.

--- Backend/task-planner-backend/src/main/java/com/task/planner/TaskPlannerBackendApplication.java
+++ Backend/task-planner-backend/src/main/java/com/task/planner/TaskPlannerBackendApplication.java
@@ -3,7 +3,11 @@ package com.task.planner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Task Planner REST API", version = "1.0"))
 public class TaskPlannerBackendApplication {
 
 	public static void main(String[] args) {
