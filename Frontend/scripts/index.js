ede660a9f885517492effec66935dc001980b19c
gauravdhamal <gauravdhamal11@gmail.com>
2023-03-26 00:17:01 +0530

    Minor bug fixed.

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -2,6 +2,8 @@ import navbar from "../components/navbar.js";
 
 document.querySelector("#navbar").innerHTML = navbar();
 
+const commonUrl = "https://task-planner-backend-production.up.railway.app";
+
 let users = "https://task-planner-backend-production.up.railway.app/users/all";
 let tasks = "https://task-planner-backend-production.up.railway.app/tasks/all";
 let sprints =
@@ -15,3 +17,40 @@ let sprints =
 //   .catch((error) => {
 //     console.error(error);
 //   });
+
+const sprintForm = document.querySelector("#sprint-form");
+
+const submitSprint = async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  let name = formData.get("sprint-name");
+  let startDate = formData.get("sprint-startdate");
+  let endDate = formData.get("sprint-enddate");
+
+  let sprintObject = {
+    name,
+    startDate,
+    endDate,
+  };
+
+  sprintObject.name = name;
+  sprintObject.startDate = startDate;
+  sprintObject.endDate = endDate;
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sprintObject),
+  };
+
+  fetch(commonUrl + "/sprints/create", options)
+    .then((response) => response.json())
+    .then((sprintObject) => console.log(sprintObject))
+    .catch((error) => console.error(error));
+};
+
+sprintForm.addEventListener("submit", submitSprint);
