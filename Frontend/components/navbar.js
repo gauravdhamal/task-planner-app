25524e108ba8410593c84cf4b93ef1e06892b95f
gauravdhamal <gauravdhamal11@gmail.com>
2023-03-25 23:15:58 +0530

    Dynamic navbar completed.

---
+++ Frontend/components/navbar.js
@@ -0,0 +1,10 @@
+let navbar = () => {
+  return `<div><h1 id="navbar-h1">Task Planner App</h1></div>
+  <div id="navbar-contents">
+    <h4><a href="index.html">Home</a></h4>
+    <h4><a href="sprints.html">Sprint</a></h4>
+    <h4><a href="tasks.html">Task</a></h4>
+  </div>`;
+};
+
+export default navbar;
---
+++ Frontend/scripts/index.js
@@ -0,0 +1,17 @@
+import navbar from "../components/navbar.js";
+
+document.querySelector("#navbar").innerHTML = navbar();
+
+let users = "https://task-planner-backend-production.up.railway.app/users/all";
+let tasks = "https://task-planner-backend-production.up.railway.app/tasks/all";
+let sprints =
+  "https://task-planner-backend-production.up.railway.app/sprints/all";
+
+fetch(sprints)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Data : ", data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
