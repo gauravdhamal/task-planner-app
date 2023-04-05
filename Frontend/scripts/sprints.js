ff56d529b9e817393116d84ad2b1ead8924a9beb
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-05 16:54:35 +0530

    Sprint creation form completed working in sprint module.

--- Frontend/scripts/sprints.js
+++ Frontend/scripts/sprints.js
@@ -7,30 +7,33 @@ const commonUrl = "http://localhost:8080";
 
 let sprintFormButtonOpen = document.getElementById("sprintFormButtonOpen");
 let sprintFormButtonClose = document.getElementById("sprintFormButtonClose");
-let sprintForm = document.getElementById("sprintForm");
+let sprintFormPost = document.getElementById("sprintFormPost");
+let postFormDiv = document.getElementById("postFormDiv");
 
 sprintFormButtonOpen.addEventListener("click", () => {
   sprintFormButtonOpen.style.display = "none";
   sprintFormButtonClose.style.display = "block";
-  sprintForm.style.display = "block";
+  sprintFormPost.style.display = "flex";
+  sprintFormPost.style.flexDirection = "column";
+  postFormDiv.style.display = "block";
 });
 
 sprintFormButtonClose.addEventListener("click", () => {
   sprintFormButtonOpen.style.display = "block";
   sprintFormButtonClose.style.display = "none";
-  sprintForm.style.display = "none";
+  sprintFormPost.style.display = "none";
+  postFormDiv.style.display = "none";
 });
 
 // To get the sprint object after creating sprint.
-
 const createSprint = async (event) => {
   event.preventDefault();
 
   let formData = new FormData(event.target);
 
-  let name = formData.get("sprint-name");
-  let startDate = formData.get("sprint-startdate");
-  let endDate = formData.get("sprint-enddate");
+  let name = formData.get("sprintName");
+  let startDate = formData.get("sprintStartdate");
+  let endDate = formData.get("sprintEnddate");
 
   let sprintObject = {
     name,
@@ -59,4 +62,32 @@ const createSprint = async (event) => {
     .catch((error) => console.error(error));
 };
 
-sprintForm.addEventListener("submit", createSprint);
+sprintFormPost.addEventListener("submit", createSprint);
+
+async function getAllSprints() {
+  let data = await fetch(commonUrl + `/sprints/all`);
+}
+
+let appendSprints = (arrayOfSprints) => {
+  const sprintTableBody = document.querySelector("#sprintTable tbody");
+  sprintTableBody.innerHTML = "";
+  arrayOfSprints.forEach((sprint) => {
+    const tr = document.createElement("tr");
+    const tdSprintId = document.createElement("td");
+    const tdName = document.createElement("td");
+    const tdStartDate = document.createElement("td");
+    const tdEndDate = document.createElement("td");
+
+    tdSprintId.textContent = sprint.sprintId;
+    tdName.textContent = sprint.name;
+    tdStartDate.textContent = sprint.startDate;
+    tdEndDate.textContent = sprint.endDate;
+
+    tr.appendChild(tdSprintId);
+    tr.appendChild(tdName);
+    tr.appendChild(tdStartDate);
+    tr.appendChild(tdEndDate);
+
+    sprintTableBody.appendChild(tr);
+  });
+};
